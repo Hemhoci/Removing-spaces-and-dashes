2110cf84e59f31d454b44e4824b491dbff0ec742
Juarren Helgerson <juarren2002@gmail.com>
2025-08-26 01:34:56 -0500

    completed the HTML document with comments, CSS, and JavaScript with comments

--- JavaScript/JavaScript.js
+++ JavaScript/JavaScript.js
@@ -1,16 +1,17 @@
-function userOutput(output){
-  const userText = output;
-  let = document.getElementById("userOutput").innerHTML = userText;
+function userOutPut(output){                     //This fuction will print converted userinput back to the user.
+  const convertedUserInput = output;            //Storing user input from removeCharacters fuction
+  let userOutput = document.getElementById("userOutput").innerHTML = convertedUserInput;   // pushing converted input back to user under id="userOutPut"
 }
 
-function removeCharacters(userin){
-  const userinput = userin;
-  const correct = userinput.replace(/[" "-]/g,"");
-  userOutput(correct);
+function removeCharacters(userin){    //this fuction will remove spaces and dashes withen a literal
+  const userinput = userin;           //storing user input into a inmutable variable name
+  const convertedInput = userinput.replace(/[" "-]/g,"");  //select characters to be removed on a globa scope
+  userOutPut(convertedInput);         //the coverted output is passed to userOutPut
 }
 
-function convert(){
-  let userinput = document.getElementsByClassName("userinput").value;
-  removeCharacters(userinput);
+function convert(){                   //This fuction will get the user input value
+  let userinput = document.getElementById("Ikey").value;    //.value is necessary to get the object value
+  removeCharacters(userinput);        //This fuction is instanciated in the block above
+                                      //userinput is passed to removeCharacters fuction 
   
 }
